eb22f4a4fba0777491911c2c3fc09a66f2bd649f
Rent Web3 <119269999+rentweb3@users.noreply.github.com>
2022-12-03 18:20:58 +0500

    changed introduction dummy collection image

--- pages/Introduction.js
+++ pages/Introduction.js
@@ -28,9 +28,10 @@ export default function Introduction(props) {
           src={
             image
               ? image
-              : "https://the-media-leader.com/wp-content/uploads/2022/07/adobestock-metaverse-resized.png"
+              : "https://camo.githubusercontent.com/0cbb62ec60fd23643b211d7ed0bb5ade6964f0da12124b95a0a3e7db096768c3/68747470733a2f2f7261772e6769746875622e636f6d2f74756c696f732f706c616365686f6c6465725f7368696d6d65722f6d61737465722f73616d706c652e676966"
           }
-          alt="bordape"
+// https://the-media-leader.com/wp-content/uploads/2022/07/adobestock-metaverse-resized.png
+          alt="loading collection"
         />
       </div>
     </div>
